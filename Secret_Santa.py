fd15fe4c7ab7ca8dcf056363ab7e626cc104eb88
paranoidlink <ben.atherton.1998@gmail.com>
2021-12-20 04:15:58 +0000

    Styalistic Improvements to make app more user friendly

--- Secret_Santa.py
+++ Secret_Santa.py
@@ -3,13 +3,17 @@ import random
 list1 = []
 list2 = []
 usrInput = ""
-print("Type the names of everyone taking part, type run to run the program once ready:")
+print("Type the name of the first person you want to add:")
 while usrInput != "run":
     usrInput = str(input())
     if usrInput == "run":
+        print("Okay running the program!")
+        print("===================================")
         break
     list1.append(usrInput)
     list2.append(usrInput)
+    print(usrInput + " added!")
+    print("Type the name of the next person you want to add or type run if you're ready to go:")
 
 while len(list1) > 0:
     choice1 = list1[random.randint(0,len(list1)) - 1]
@@ -21,4 +25,5 @@ while len(list1) > 0:
         print(choice1 + " Has to give a present to " + choice2)
         list1.remove(choice1)
         list2.remove(choice2)
+print("===================================")
 exit()
